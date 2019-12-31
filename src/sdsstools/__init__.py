4711dec5126b263526b231474f424d23e90bc98a
José Sánchez-Gallego <gallegoj@uw.edu>
2019-12-30 21:38:11 -0800

    Add package information

---
+++ src/sdsstools/__init__.py
@@ -0,0 +1,12 @@
+
+from .metadata import get_package_version
+from .logger import get_logger
+
+
+__version__ = get_package_version(path=__file__, package_name='sdsstools')
+
+NAME = 'sdsstools'
+
+
+# Get a logger, mostly for the nice colouring.
+log = get_logger(NAME)
