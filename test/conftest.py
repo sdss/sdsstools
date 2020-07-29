8f9f43e12255d15e0e934b46ce98ad7cd6900d64
José Sánchez-Gallego <gallegoj@uw.edu>
2020-07-29 14:34:38 -0700

    Add test framework and tests for configuration

---
+++ test/conftest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2020-07-29
+# @Filename: conftest.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
