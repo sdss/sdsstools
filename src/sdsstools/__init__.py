35300e33956942edd09fbbb22da817fec2eb733e
José Sánchez-Gallego <gallegoj@uw.edu>
2023-11-09 13:34:16 -0800

    Import get_sjd at top level

--- src/sdsstools/__init__.py
+++ src/sdsstools/__init__.py
@@ -1,4 +1,12 @@
-# isort:skip_file
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2023-11-09
+# @Filename: __init__.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
 
 import sys
 
@@ -10,10 +18,12 @@ __version__ = get_package_version(path=__file__, package_name="sdsstools")
 NAME = "sdsstools"
 
 
+from ._vendor import color_print, toml
 from .configuration import *
 from .logger import *
 from .metadata import *
-from ._vendor import color_print, toml
+from .time import get_sjd
+
 
 # This is a hack to allow doing from sdsstools.color_print import color_text
 # which some code already does.
